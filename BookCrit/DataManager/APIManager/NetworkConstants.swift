4fad2dedaec3d1ad3322af90d39e41f39c650595
Fitgerald Afful <fitzafful@gmail.com>
2020-10-14 01:40:06 +0000

    Added Fonts to project and modified Search item fonts

---
+++ BookCrit/DataManager/APIManager/NetworkConstants.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkConstants.swift
+//  BookCrit
+//
+//  Created by Fitzgerald Afful on 14/10/2020.
+//  Copyright © 2020 Glivion. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkingConstants {
+    static let baseUrl = "https://api.masterprophetlibrary.com/v1/"
+    static let googleBooksBaseUrl = "https://www.googleapis.com/books/v1"
+
+    static let googleBooksKey = "AIzaSyCkOPHCiPpM4aLb0-kuOLSMzLWKfrMtgVU"
+    static let networkErrorMessage = "Please check your internet connection and try again."
+}
