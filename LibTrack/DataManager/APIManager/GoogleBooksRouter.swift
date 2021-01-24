3c1ef7302e0a503e2b04261abb8a1f084589333a
Fitgerald Afful <fitzafful@gmail.com>
2021-01-24 00:06:25 +0000

    Renamed Project and Relevant files to LibTrack from BookCrit

---
+++ LibTrack/DataManager/APIManager/GoogleBooksRouter.swift
@@ -0,0 +1,80 @@
+//
+//  GoogleBooksRouter.swift
+//  LibTrack
+//
+//  Created by Fitzgerald Afful on 13/10/2020.
+//  Copyright © 2020 Glivion. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+enum GoogleBooksRouter: APIConfiguration {
+
+    case searchBookOnGoogleBooks(book: String)
+    case getBookDetails(volumeId: String)
+
+    internal var method: HTTPMethod {
+        switch self {
+        case .searchBookOnGoogleBooks: return .get
+        case .getBookDetails: return .get
+        }
+    }
+
+    internal var path: String {
+        switch self {
+        case .searchBookOnGoogleBooks: return NetworkingConstants.googleBooksBaseUrl + "/volumes"
+        case .getBookDetails(let volumeId): return NetworkingConstants.googleBooksBaseUrl + "/volumes/\(volumeId)"
+        }
+    }
+
+    internal var parameters: [String: Any] {
+        switch self {
+        case .searchBookOnGoogleBooks(let book):
+            return ["q": book,
+                    "orderBy": "relevance",
+                    "key": NetworkingConstants.googleBooksKey]
+        default:
+            return ["key": NetworkingConstants.googleBooksKey]
+        }
+    }
+
+    internal var body: [String: Any] {
+        switch self {
+        default: return [:]
+        }
+    }
+
+    internal var headers: HTTPHeaders {
+        switch self {
+        default: return ["Content-Type": "application/json", "Accept": "application/json"]
+        }
+    }
+
+    func asURLRequest() throws -> URLRequest {
+        var urlComponents = URLComponents(string: path)!
+        var queryItems: [URLQueryItem] = []
+        for item in parameters {
+            queryItems.append(URLQueryItem(name: item.key, value: "\(item.value)"))
+        }
+        if !(queryItems.isEmpty) {
+            urlComponents.queryItems = queryItems
+        }
+        let url = urlComponents.url!
+        var urlRequest = URLRequest(url: url)
+
+        print("URL: \(url)")
+        print(parameters)
+        print(body)
+
+        urlRequest.httpMethod = method.rawValue
+        urlRequest.allHTTPHeaderFields = headers.dictionary
+
+        if !(body.isEmpty) {
+            urlRequest = try URLEncoding().encode(urlRequest, with: body)
+            let jsonData1 = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
+            urlRequest.httpBody = jsonData1
+        }
+        return urlRequest
+    }
+}
--- Podfile
+++ Podfile
@@ -1,32 +1,22 @@
 source 'https://github.com/CocoaPods/Specs.git'
 
-platform :ios, ‘11.0’
+platform :ios, '13.0'
 use_frameworks!
 inhibit_all_warnings!
 
-target ‘BookCrit’ do
+target 'LibTrack' do
     pod 'Nuke'
-    pod 'NSDate+TimeAgo'
     pod 'IQKeyboardManagerSwift'
     pod 'Alamofire'
+    pod 'Firebase/Auth'
     pod 'Firebase/Core'
     pod 'Firebase/Messaging'
-    pod 'Firebase/Database'
+    pod 'GoogleSignIn'
     pod 'ESPullToRefresh'
     pod 'FTIndicator'
-    pod 'GoogleSignIn'
-    pod 'Firebase/Auth'
-    pod 'SkeletonView'
-    pod 'DropDown'
-    pod 'Crashlytics'
-    pod 'Fabric'
-    pod 'GrowingTextView'
-    pod 'IQKeyboardManagerSwift'
-    pod 'Crashlytics'
-    pod "DTPhotoViewerController"
-    pod 'SwiftLint'
+    pod 'DTPhotoViewerController'
 		
-		target 'BookCritTests' do
+		target 'LibTrackTests' do
         inherit! :search_paths
         pod 'Firebase'
     end
